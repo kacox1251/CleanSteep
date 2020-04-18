d1cc5fa562335a77038668100f8bee7fdc45b5ab
Valerie Riley <valeriesriley@gmail.com>
2020-04-17 23:10:45 -0600

    animation only occurs on the home page

--- client/src/components/Image/index.js
+++ client/src/components/Image/index.js
@@ -6,11 +6,15 @@ import { TweenMax, Linear } from "gsap";
 export function Image(props) {
   let imgRef = useRef(null);
 
+
+
   useEffect(() => {
-    TweenMax.fromTo(imgRef, 1, {x:25, y:340}, {x:60, y:300});
-    TweenMax.fromTo(imgRef, 1, {x:60, y:300}, {x:20, y:240, delay: 1});
-    TweenMax.fromTo(imgRef, 1, {x:20, y:240}, {x:70, y:150, delay: 2});
-    TweenMax.fromTo(imgRef, 1, {x:70, y: 150}, {x:0, y:0, delay: 3});
+    if (window.location.pathname == '/') {
+      TweenMax.fromTo(imgRef, 1, {x:25, y:340}, {x:60, y:300});
+      TweenMax.fromTo(imgRef, 1, {x:60, y:300}, {x:20, y:240, delay: 1});
+      TweenMax.fromTo(imgRef, 1, {x:20, y:240}, {x:70, y:150, delay: 2});
+      TweenMax.fromTo(imgRef, 1, {x:70, y: 150}, {x:0, y:0, delay: 3});
+    }
   })
 
     return (
