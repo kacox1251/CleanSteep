4ceb53b19bdb50d68441852c47ed492409e0fd6d
kacox1251 <56182547+kacox1251@users.noreply.github.com>
2020-04-20 20:54:09 -0600

    creating build

---
+++ client/build/precache-manifest.69feb93b56087c4ce027aa58ec8cc1ea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6e3ca1da8e9919333541",
+    "url": "/static/css/main.1508a70f.chunk.css"
+  },
+  {
+    "revision": "6e3ca1da8e9919333541",
+    "url": "/static/js/main.3d03466b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "0fbe4b4886799ba4b70b",
+    "url": "/static/js/2.968a938e.chunk.js"
+  },
+  {
+    "revision": "50b3b7b43211e4faf9f4b60f47dabfc1",
+    "url": "/static/media/Rocks.50b3b7b4.jpg"
+  },
+  {
+    "revision": "f14a003a58b622adc9c36a84beb0cb66",
+    "url": "/index.html"
+  }
+];
