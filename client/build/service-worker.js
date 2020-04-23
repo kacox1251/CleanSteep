bf5a45e18e76e9cef8e724478654e8fe281da9b6
Valerie Riley <valeriesriley@gmail.com>
2020-04-22 18:27:05 -0600

    fixed spacing on search page

--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2042ad53f8fb6f29d5df189201390ed5.js"
+  "/precache-manifest.de06edb74eca8ddacf7528cca5e8b5c4.js"
 );
 
 workbox.clientsClaim();
@@ -30,5 +30,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute("/index.html", {
   
-  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
